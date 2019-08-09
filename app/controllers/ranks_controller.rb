7d8f3bbaec5ad3426e92e70d65c98d54da491c38
Makoto Naruse <mako_marimo@yahoo.co.jp>
2019-08-09 10:28:57 +0900

    順位を調整

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -53,6 +53,10 @@ class RanksController < ApplicationController
     ranks.destroy_all
     for num in 1..10 do
       if ranks[num] != 0
+        rank = Rank.find_by(race_id: race.id, rank: num)
+        if rank
+          rank.destroy
+        end
         rank = Rank.new(race_id: race.id, rane: ranks[num], rank: num)
         rank.save
       end
