3324706a474a7d194673eb9c3793f2e328dab38d
Makoto Naruse <mako_marimo@yahoo.co.jp>
2020-05-15 23:14:46 +0900

    並び順修正

--- app/models/race.rb
+++ app/models/race.rb
@@ -1,7 +1,7 @@
 class Race < ApplicationRecord
-  has_many :combinations, -> { order(:rane => :desc) }, foreign_key: :race_id, dependent: :destroy
+  has_many :combinations, -> { order(:rane) }, foreign_key: :race_id, dependent: :destroy
   has_many :combination_fours, foreign_key: :race_id, dependent: :destroy
-  has_many :ranks, -> { order(:rane => :desc) }, foreign_key: :race_id, dependent: :destroy
-  has_many :results, -> { order(:rane => :desc) }, foreign_key: :race_id, dependent: :destroy
+  has_many :ranks, -> { order(:rane) }, foreign_key: :race_id, dependent: :destroy
+  has_many :results, -> { order(:rane) }, foreign_key: :race_id, dependent: :destroy
 
 end
