5eda709f0a954238e50aa65bdfe4166a10e968e0
Makoto Naruse <mako_marimo@yahoo.co.jp>
2020-07-16 20:01:31 +0900

    モデル変更

--- app/models/university.rb
+++ app/models/university.rb
@@ -1,8 +1,10 @@
 class University < ApplicationRecord
+  has_secure_password
+  
   has_many :fours
 
   validates :u_name, presence: true
   validates :read, presence: true
   validates :erea, presence: true
-  validates :password, presence: true
+  # validates :password, presence: true
 end
